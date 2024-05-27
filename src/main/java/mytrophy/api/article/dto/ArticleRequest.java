c6d86f64dcb5b88f17670b2409a93788bc5917f0
soonduck <soonduck3729@gmail.com>
2024-05-27 22:03:25 +0900

    feat: member 연관관계 추가

--- src/main/java/mytrophy/api/article/dto/ArticleRequest.java
+++ src/main/java/mytrophy/api/article/dto/ArticleRequest.java
@@ -3,17 +3,18 @@ package mytrophy.api.article.dto;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import mytrophy.api.article.enumentity.Header;
+import mytrophy.api.member.entity.Member;
 
 @Data
+@NoArgsConstructor
 public class ArticleRequest {
     private Header header;
     private String name;
     private String content;
     private String imagePath;
-
-    public ArticleRequest() {
-    }
+    private Member member;
 
     @Builder
     public ArticleRequest(Header header, String name, String content) {
