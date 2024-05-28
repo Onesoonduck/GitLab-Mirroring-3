01a0bdb33b8c7bf0cf8bb51523fe8c0b83ea31f6
soonduck <soonduck3729@gmail.com>
2024-05-28 15:15:41 +0900

    feat: member 추가

--- src/main/java/mytrophy/api/article/entity/Article.java
+++ src/main/java/mytrophy/api/article/entity/Article.java
@@ -36,9 +36,9 @@ public class Article extends BaseEntity {
 
 
     //TODO : member 연관관계 관련 로직 추가해야 함
-//    @ManyToOne(fetch = FetchType.LAZY) // 지연 로딩 설정
-//    @JoinColumn(name = "member_id")
-//    private Member member; // 회원
+    @ManyToOne(fetch = FetchType.LAZY) // 지연 로딩 설정
+    @JoinColumn(name = "member_id")
+    private Member member; // 회원
 
     //TODO : comment 연관관계 관련 로직 추가해야 함
 //    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
